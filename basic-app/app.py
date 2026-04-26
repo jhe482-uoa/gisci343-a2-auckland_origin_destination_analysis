c2cef81d480794fce42d9eea534498e1e5f78cbf
Jeff He <jhe482@aucklanduni.ac.nz>
2026-04-26 22:10:03 +1200

    Updated map displaying function

--- basic-app/app.py
+++ basic-app/app.py
@@ -3,28 +3,26 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from shinywidgets import output_widget, render_widget
 import geopandas as gpd
-from ipyleaflet import Map, GeoData, LayersControl, basemaps, basemap_to_tiles, display
+from ipyleaflet import Map, GeoData, LayersControl, basemaps, basemap_to_tiles, display, CircleMarker
 
 
 # Inline sample data (replace with real data next week)
 sa2_data = pd.read_csv(r"data\statsnz-2023-census-main-means-of-travel-to-work-by-statistical-area-CSV\2023-census-main-means-of-travel-to-work-by-statistical-area.csv")
 sa2_data.replace(-999,0,inplace=True)
-sa2_data = sa2_data[(sa2_data["2023_Total_stated"] > 0) | (sa2_data["2018_Total_stated"] > 0)]
+# sa2_data = sa2_data[(sa2_data["2023_Total_stated"] > 0) | (sa2_data["2018_Total_stated"] > 0)]
 
 path = r"data\statsnz-statistical-area-2-2023-generalised-SHP\statistical-area-2-2023-generalised.shp"
 sa2shape2023 = gpd.read_file(path)
-new_tiles = basemap_to_tiles(basemaps.CartoDB.Positron)
 
 sa2shape2023 = sa2shape2023.to_crs(epsg=4326)
+
+sa2shape2023['SA22023_V1'] = sa2shape2023['SA22023_V1'].astype(int)
+
+merged2023 = sa2shape2023.merge(sa2_data,"left", left_on="SA22023_V1", right_on="SA22023_V1_00_usual_residence_address")
+print(merged2023)
 center_x = sa2shape2023.geometry.centroid.x.mean()
 center_y = sa2shape2023.geometry.centroid.y.mean()
-map = Map(center=(center_y, center_x), zoom=5, layers=(new_tiles, ))
 
-geo_data = GeoData(geo_dataframe=sa2shape2023, style={'color': 'white', 'fillOpacity': 0.3, 'weight': 1},
-    name='NZ Regions')
-
-map.add_layer(geo_data)
-display(map)
 
 
 app_ui = ui.page_sidebar(
@@ -38,15 +36,15 @@ app_ui = ui.page_sidebar(
         ),
     ui.navset_card_tab(
         ui.nav_panel(
-            "Work + Education",
+            "Work",
             ui.h2("Maps"),
-            # output_widget("both_display"),
+            output_widget("map_display"),
             ui.output_table("tbl")
         ),
         ui.nav_panel(
             "Test",
             ui.h2("Maps"),
-            output_widget("map_display"),
+            
         )
     )
     )
@@ -57,10 +55,12 @@ def server(input, output, session):
     @reactive.event(input.update)
     def filter():
         if input.mode() == "Leaving From":
-            filtered = sa2_data[sa2_data["SA22023_V1_00_NAME_ASCII_usual_residence_address"] == input.selected_sa2()]
+            # print(input.selected_sa2())
+            filtered = merged2023[merged2023["SA22023_V1_00_NAME_ASCII_usual_residence_address"] == input.selected_sa2()]
             return filtered
         else:
-            filtered = sa2_data[sa2_data["SA22023_V1_00_NAME_ASCII_workplace_address"] == input.selected_sa2()]
+            # print(input.selected_sa2())
+            filtered = merged2023[merged2023["SA22023_V1_00_NAME_ASCII_workplace_address"] == input.selected_sa2()]
             return filtered
 
     @render.text
@@ -70,15 +70,70 @@ def server(input, output, session):
             return "No suburbs match the current filter."
         return f"{len(df)} suburbs, mean income ${df['median_income'].mean():,.0f}."
 
+    @reactive.Effect
+    @reactive.event(input.reset)
+    def _():
+        if len(map_display.widget.layers) > 2:
+            map_display.widget.layers = map_display.widget.layers[:2]
+        
+        map_display.widget.center = (center_y, center_x)
+        map_display.widget.zoom = 5
+
     @render.table
+    @reactive.event(input.update)
     def tbl():
-        return filter()
+        return filter()[["SA22023_V1_00_NAME_ASCII_usual_residence_address", "SA22023_V1_00_NAME_ASCII_workplace_address","2018_Total_stated", "2023_Total_stated"]]
 
 
     @render_widget
     def map_display():
-        m = Map(center=(-36.8485, 174.7633), zoom=10, basemap=basemaps.CartoDB.Positron)
+        new_tiles = basemap_to_tiles(basemaps.CartoDB.Positron)
+        
+        geo_data = GeoData(
+            geo_dataframe=sa2shape2023, 
+            style={'color': 'grey', 'fillOpacity': 0.3, 'weight': 1},
+            name='NZ Regions'
+        )
+
+        m = Map(center=(center_y, center_x), zoom=5, layers=(new_tiles,))
+        m.add_layer(geo_data)
+
         return m
+
+
+    @reactive.Effect
+    @reactive.event(input.update)
+    def _():
+        selected_sa2 = sa2shape2023[sa2shape2023["SA22023__1"] == input.selected_sa2()]
+        lat = selected_sa2.geometry.centroid.y.values[0]
+        lon = selected_sa2.geometry.centroid.x.values[0]
+        
+        marker = CircleMarker(
+            location=(lat, lon), 
+            radius=5, 
+            color="yellow", 
+            fill_color="yellow", 
+            fill_opacity=1.0,
+            name="Selected Centre"
+        )
+        filtered = filter()
+        if input.mode() == "Leaving From":
+            target_ids = filtered["SA22023_V1_00_workplace_address"].unique()
+            filtered = merged2023[merged2023["SA22023_V1"].isin(target_ids)].drop_duplicates(subset="SA22023_V1")
+
+        new_layer = GeoData(
+            geo_dataframe=filtered,
+            style={'color': 'red', 'fillOpacity': 0.5},
+            name="Filtered Results"
+        )
+        
+        if len(map_display.widget.layers) > 2:
+            map_display.widget.layers = map_display.widget.layers[:2]
+        
+        map_display.widget.add_layer(new_layer)
+        map_display.widget.add_layer(marker)
+        map_display.widget.center = (lat, lon)
+        map_display.widget.zoom = 8
 
     @render.plot
     def chart():
